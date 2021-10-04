a8b922af0069bf028648b27c05244dc3563438b2
Suwath Ch <suwatch@microsoft.com>
2021-10-04 14:17:03 -0700

    Robust to the test to check version

--- dotnetcore30web/Startup.cs
+++ dotnetcore30web/Startup.cs
@@ -44,11 +44,8 @@ namespace dotnetcore30web
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseCookiePolicy();
